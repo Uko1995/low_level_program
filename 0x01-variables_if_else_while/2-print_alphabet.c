de67d6d9b04020d4dbd9e21dd36d9ed8b939b57a
Uko Uwatt <uwattuko@gmail.com>
2023-02-17 11:38:23 -0800

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,8 +9,8 @@ int main(void)
 {
 	char lower_case;
 
-	for (lower_case = 'a'; lowercase = 'z'; lower_case++)
-		putchar(lower_case);
-	putchar("\n");
+	for (lower_case = 'a'; lower_case = 'z'; lower_case++)
+		putchar("%c\n", lower_case);
+
 	return (0);
 }
