1f2592a5254c45ad7d4929b837bb754075ea1916
Uko Uwatt <uwattuko@gmail.com>
2023-02-17 13:21:40 -0800

    print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int num;
+	char la;
+
+	for (num = 0; num < 10; num++)
+		putchar(num);
+	for (la = 'a', la = 'f'; la++)
+		putchar(la);
+	putchar('\n');
+	return (0);
+}
