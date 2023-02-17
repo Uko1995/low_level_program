f2e145b57db6facacee1cfd556c9d524bd038c3d
Uko Uwatt <uwattuko@gmail.com>
2023-02-17 11:34:04 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lowercase
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char lower_case;
+
+	for (lower_case = 'a'; lowercase = 'z'; lower_case++)
+		putchar(lower_case);
+	putchar("\n");
+	return (0);
+}
