dd41b154e1feb0053ed1fc2bd57edb9a1ffe2776
Uko Uwatt <uwattuko@gmail.com>
2023-02-17 12:24:49 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints all digits in base 10
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d\n", num);
+	return (0);
