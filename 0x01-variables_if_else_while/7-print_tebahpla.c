678666d47a0116746a5a542a47bb3748193cad7d
Uko Uwatt <uwattuko@gmail.com>
2023-02-17 12:56:25 -0800

    print tebalpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char e;
+
+	e = 'z';
+	for (; e = 'a'; a--)
+		putchar(e);
+	putchar('\n');
+	return (0);
+}
