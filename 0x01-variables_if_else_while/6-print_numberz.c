d7584aaab96eee12749a09498e8b4ae9bc6331d5
Uko Uwatt <uwattuko@gmail.com>
2023-02-17 12:45:49 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = 0; d < 10; d++)
+		putchar(d);
+	putchar('\n');
+	return (0);
+}
