e8f7ccc0da7b0389820a2e59de1e599748007e64
Uko Uwatt <uwattuko@gmail.com>
2023-02-21 14:12:53 -0800

    aphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet in lowercase 10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int a, int b;
+
+	for (a = 1; a < 10; a++)
+	{
+		for (b = 'a'; b <= 'z'; b++)
+			_putchar(b);
+		_putchtar(a);
+	}
+}
