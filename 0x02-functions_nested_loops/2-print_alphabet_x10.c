8014d90e601c0c40e2d47508c9321d026d2c42d1
Uko Uwatt <uwattuko@gmail.com>
2023-02-21 14:16:40 -0800

    alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,12 +8,12 @@
 
 void print_alphabet_x10(void)
 {
-	int a, int b;
+	char b, a;
 
 	for (a = 1; a < 10; a++)
 	{
 		for (b = 'a'; b <= 'z'; b++)
 			_putchar(b);
-		_putchtar(a);
+		_putchar('\n');
 	}
 }
