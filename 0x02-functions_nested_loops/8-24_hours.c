a054092f5b7b65063e5e3088a8c1eec3957e3139
Uko Uwatt <uwattuko@gmail.com>
2023-02-21 16:14:58 -0800

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int m, n;
+
+	for (m = 0; m <= 23; m++)
+	{
+		for (n = 0; n <= 59; n++)
+		{
+			_putchar(m / 10 + '0');
+			_putchar(m % 10 + '0');
+			_putchar(':');
+			_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
