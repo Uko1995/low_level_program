4cc24d798548def505b11b8567eab14aca1a92f2
Uko Uwatt <uwattuko@gmail.com>
2023-02-21 16:45:57 -0800

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,23 +8,28 @@
 
 void times_table(void)
 {
-	int i;
+	int a, b, op;
 
-	for (i = 0; i <= 12; i++)
+	for (a = 0; a <= 9; a++)
 	{
-		int result;
 
-		result = 9 * i;
-		_putchar('9');
-		_putchar(' ');
-		_putchar('x');
-		_putchar(' ');
-		_putchar(i + '0');
-		_putchar(' ');
-		_putchar('=');
-		_putchar(' ');
-		_putchar(result / 10 + '0');
-		_putchar(result % 10 + '0');
+		_putchar(48);
+		for (b = 1; b <= 9; b++)
+		{
+			op = a * b;
+			_putchar(44);
+			_putchar(32);
+			if (op <= 9)
+			{
+				_putchar(32);
+				_putchar(op + 48);
+			}
+			else
+			{
+				_putchar((op / 10) + 48);
+				_putchar((op % 10) + 48);
+			}
+		}
 		_putchar('\n');
 	}
 }
