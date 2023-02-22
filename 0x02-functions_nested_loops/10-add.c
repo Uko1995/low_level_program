20f1d54fdc1c9a5141d59d72a872ce779aa3c6da
Uko Uwatt <uwattuko@gmail.com>
2023-02-21 16:52:17 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @a: integer to be added
+ * @b: integer to be added
+ *
+ * Return: result
+ */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+	_putchar('\n');
+}
