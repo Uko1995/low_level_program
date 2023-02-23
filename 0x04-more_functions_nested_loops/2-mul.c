d4ec82c24405c3e8b43916acb7cad76eeb086e20
Uko Uwatt <uwattuko@gmail.com>
2023-02-23 13:47:56 -0800

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	result = a * b;
+	return (0);
+}
