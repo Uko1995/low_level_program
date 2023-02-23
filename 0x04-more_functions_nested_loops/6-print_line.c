0fd0a3b69b14a45412d45fcee1c4ab8101e83d40
Uko Uwatt <uwattuko@gmail.com>
2023-02-23 14:58:55 -0800

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - entry point
+ *@n: integer n
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int a;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int a;
+
+		for (a = 1; a <= n; a++)
+			_putchar('_');
+	_putchar('\n');
+	}
+}
