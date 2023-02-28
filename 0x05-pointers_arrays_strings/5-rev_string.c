c84a52bc5beb3230e01b6454ea63376ea3087138
Uko Uwatt <uwattuko@gmail.com>
2023-02-27 20:03:19 -0800

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string input
+ *
+ * Return: 0
+ */
+
+void rev_string(char *s)
+{
+	int len, i;
+	char temp;
+
+	len = 0;
+	while (s[len] != '\0')
+		len++
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+	return (0);
+}
