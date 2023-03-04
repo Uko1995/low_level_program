a0e28cf9a5961f7475b607c13b46cf434c5edce1
Uko Uwatt <uwattuko@gmail.com>
2023-03-03 16:39:40 -0800

    concatenate two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,14 +14,14 @@ char *_strcat(char *dest, char *src)
 
 	i = 0;
 	while (dest[i] != '\0')
-		dest++;
+		i++;
 
 	j = 0;
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
-		dest++;
-		src++;
+		i++;
+		j++;
 	}
 	dest[i] = '\0';
 	return (dest);
