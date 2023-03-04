a9630ab52e78eaf34672b41f0d84111a5d8fd93a
Uko Uwatt <uwattuko@gmail.com>
2023-03-03 17:23:58 -0800

    concatenate two strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *_strncat - concatenates two strings
+ * @dest: input
+ * @src: input
+ * @n: input
+ *
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,3 +1,5 @@
 pointers, arrays and strings
 0-strcat.c
 concatenate two strings and add a null chaacter at the end, return pointer to the string
+1-strncat.c
+> concatenates two strings and adda a null character, returns pointer to string
