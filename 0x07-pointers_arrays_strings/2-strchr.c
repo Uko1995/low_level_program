726cba76f2a6602182a7d1be289fa80808cb1ad3
Uko Uwatt <uwattuko@gmail.com>
2023-03-06 14:15:20 -0800

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strchr - function locats a character in a string
+ * @s: string input
+ * @c: character to be located
+ *
+ * Return: pointer to the first occurence of c in s
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	i = 0;
+
+	while (s[i] < '\0')
+	{
+		if (s[i] == c)
+			return (&s[i]);
+		else if (s[i] != c)
+			return (0);
+	}
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -3,3 +3,5 @@ Even more pointers, strings and arrays
 a function that fills memory with a constant byte.
  1-memcpy.c
 function copies memory area.
+2-strchr.c
+function locates a character in a string
