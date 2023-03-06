ea29d094c93ce5f33173207b44977e70b64c075d
Uko Uwatt <uwattuko@gmail.com>
2023-03-06 14:43:44 -0800

    strchr#

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -14,12 +14,14 @@ char *_strchr(char *s, char c)
 
 	i = 0;
 
-	while (s[i] <= '\0')
+	while (s[i] != '\0')
 	{
-		if (s[i] == c && s[i] == '\0')
+		if (s[i] == c)
 		return (&s[i]);
 		i++;
 	}
+	if (s[i] == '\0')
+		return (&s[i]);
 
-	return (NULL);
+	return (0);
 }
