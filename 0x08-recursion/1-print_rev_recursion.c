7963ea1324a6d0e68d5e113bd5d7c68dcf9b09d3
Uko Uwatt <uwattuko@gmail.com>
2023-03-08 16:03:41 -0800

    1-print_rev_recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: pointer input
+ *
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	unsigned int n = 0;
+
+	if (s[n] == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(s[i]);
+	}
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,3 +1,5 @@
 Recursion
 0-puts_recursion.c
 function prints a string followed by a new line.
+1-print_rev_recursion.c
+function prints a string in reverse.
