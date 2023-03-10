92e0b299096621897b64da9766e8db3bf093a951
Uko Uwatt <uwattuko@gmail.com>
2023-03-10 13:04:38 -0800

    print number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the number of arguments followed ba a new line
+ * @argc: number of arguments
+ * @argv: arguments vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,3 +1,5 @@
 argc_argv
 0-whatsmyname.c
 prints the name of the program followed by a new line
+1-args.c
+prints the number of arguments followed by a new line
