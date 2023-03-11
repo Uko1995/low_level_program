ce1a0e68172f7ecbfd94a6a7b40d894e1e4abe21
Uko Uwatt <uwattuko@gmail.com>
2023-03-10 17:51:14 -0800

    multiply numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,9 +12,13 @@
 
 int main(int argc, char *argv[])
 {
+	int result;
+
+	result = atoi(argv[1] * argv[2]);
+
 	if (argc == 3)
 	{
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		printf("%d\n", result);
 	}
 	else
 	{
