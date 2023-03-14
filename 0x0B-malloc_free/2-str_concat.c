212912e1da81f8f9c4b5ed53668cade2dd7fdcaa
Uko Uwatt <uwattuko@gmail.com>
2023-03-14 07:03:46 -0700

    concatenate strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,10 +14,11 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, j, k;
 	char *new;
 
-	if ( s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+	if (s1 == NULL && s2 == NULL)
+		s1 = s2 = "";
+
+	i = j = 0;
+
 	while (s1[i] != '\0')
 		i++;
 	while (s2[j] != '\0')
