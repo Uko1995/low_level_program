4b4ec6cfaf100b55afd22a57a08b7838a5e3f7e5
Uko Uwatt <uwattuko@gmail.com>
2023-03-14 20:22:51 -0700

    split strings

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,40 +1,91 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
-/**
- * argstostr - concatenates all the arguments of your program.
- * @ac: number of arguments
- * @av: double pointer to arguments
- *
- * Return:pointer to new string, or NULL if error
- */
-char *argstostr(int ac, char **av)
-{
-	int i, j, k = 0, n = 0;
-	char *s;
 
-	if (ac <= 0 || av == NULL)
-		return (NULL);
-	for (i = 0; i < ac; i++)
+/**
+ * helper - helps function
+ * @word: wordcount
+ * @len: length
+ * @str: string to go through
+ * @s: array you are assigning
+ *
+ * Return: char value
+ */
+
+
+char **helper(int word, int len, char *str, char **s)
+{
+	int i, k, j;
+
+	j = 0;
+	for (i = 0; i < word; i++)
 	{
-		for (j = 0; av[i][j]; j++)
-			n++;
-		n++;
+		k = 0;
+		for (; j < len; j++)
+		{
+			if (str[0] != ' ' || str[j] != ' ')
+			{
+				s[i][k] = str[j];
+				k++;
+			}
+			if (j != 0 && str[j] == ' ' && str[j - 1] != ' ')
+			{
+				j++;
+				break;
+			}
+		}
+		s[i][k + 1] = '\0';
 	}
-	n++;
-	s = malloc(n * sizeof(char));
+	s[word + 1] = NULL;
+	return (s);
+}
+/**
+ * strtow - string to words
+ * @str: string to check
+ * Return: return char value
+ */
+
+char **strtow(char *str)
+{
+	int len, i, j, size, k, word;
+	char **s;
+
+	if (str == NULL)
+		return (NULL);
+	len = 0;
+	word = 0;
+	while (str[len] != '\0')
+	{
+		if (str[0] != ' ')
+		word++;
+		if (str[len] != ' ' && str[len - 1] == ' ' && len != 0)
+			word++;
+		len++;
+	}
+	s = (char **)malloc(sizeof(char *) * word + 1);
 	if (s == NULL)
 		return (NULL);
-	for (i = 0; i < ac; i++)
+	j = 0;
+	for (i = 0; i < word; i++)
 	{
-		for (j = 0; av[i][j]; j++)
+		size = 0;
+		for (; j < len; j++)
 		{
-			s[k] = av[i][j];
-		k++;
+			if (str[0] != ' ' || str[j] != ' ')
+				size++;
+			if (str[j] == ' ' && size > 0)
+				break;
 		}
-		s[k] = '\n';
-		k++;
+		printf("%d\n", size);
+		s[i] = (char *)malloc(sizeof(char) * size + 1);
+		if (s[i] == NULL)
+		{
+			for (k = i - 1; k >= 0; k--)
+				free(s[k]);
+			free(s);
+		}
 	}
-	s[k] = '\0';
+	s = helper(word, len, str, s);
 	return (s);
 }
