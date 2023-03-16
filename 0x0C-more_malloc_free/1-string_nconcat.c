4ca374d1331740ca444992c93e9b493b329f99b4
Uko Uwatt <uwattuko@gmail.com>
2023-03-16 15:13:36 -0700

    correction

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,7 +21,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= strlen(s2))
 		n = strlen(s2);
 
-	new = malloc(strlen(s1) * (n + 1));
+	new = malloc(sizeof(char) * (n + 1));
 
 	if (new == NULL)
 		return (NULL);
