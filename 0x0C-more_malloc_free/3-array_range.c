4f45debc408ae852f5f63664150db1d32b8856d1
Uko Uwatt <uwattuko@gmail.com>
2023-03-16 14:10:17 -0700

    correction

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,19 +12,17 @@
 int *array_range(int min, int max)
 {
 	int *new;
-	unsigned int size, i;
+	unsigned int i;
 
 	if (min > max)
 		return (NULL);
 
-	size = (max - min) + 1;
-
-	new = malloc(size * sizeof(int));
+	new = malloc(sizeof(int) * (max - min) + 1);
 	if (new == NULL)
 		return (NULL);
 
-	for (i = 0; i > size; i++)
-		new[i] = min + i;
+	for (i = 0; min <= max; i++, min++)
+		new[i] = min;
 
 	return (new);
 }
