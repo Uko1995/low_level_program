616c90c8176864ad17f0067541df286870d35f64
Uko Uwatt <uwattuko@gmail.com>
2023-03-20 19:26:55 -0700

    creates a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog
+ * @name: new name
+ * @age: new age
+ * @owner: new owner
+ *
+ * Return: pointer to new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
+
+	d->name = malloc(strlen(name) + 1);
+	d->owner = malloc(strlen(owner) + 1);
+	if (d->name == NULL || d->owner == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+
+	strcpy(d->name, name);
+	strcpy(d->owner, owner);
+	d->age = age;
+
+	return (d);
+}
--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -9,3 +9,5 @@ Write a function that initialize a variable of type struct dog
 Write a function that prints a struct dog
 dog.h 
 Define a new type dog_t as a new name for the type struct dog
+4-new_dog.c 
+creates a new dog
