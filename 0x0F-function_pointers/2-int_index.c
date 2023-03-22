f9d231b923747818fec0f6dff80bfacb50a8b354
Uko Uwatt <uwattuko@gmail.com>
2023-03-22 09:48:39 -0700

    Write a function that searches for an integer.

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index -  searches for an integer
+ * @array: ponter to array
+ * @size: size of array
+ * @cmp: function pointer
+ *
+ * Return: index of first element with an integer
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
+
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (i);
+}
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -6,3 +6,5 @@ write a function that prints a name.
 
 1-array_iterator.c
 function that executes a function given as a parameter on each element of an array
+2-int_index.c
+Write a function that searches for an integer.
