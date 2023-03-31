e816dd71ec225d91aa7c8d1394b1a88c4eaa6324
Uko Uwatt <uwattuko@gmail.com>
2023-03-30 21:03:50 -0700

    print all elements of list_t list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all elements of list_t list
+ * @h: pointer to list_t list
+ *
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned int number_of_nodes = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		number_of_nodes++;
+	}
+	return (number_of_nodes);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,4 @@
+Singly linked lists
+
+0-print_list.c 
+function prints all the elements of list_t list
