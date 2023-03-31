ce030a88450fd5eb7ab78a71045e9df25d8a422d
Uko Uwatt <uwattuko@gmail.com>
2023-03-30 22:36:57 -0700

    prints quote before main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * first - prints a qoute before the main function
+ *
+ * Return: void
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
