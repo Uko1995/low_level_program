c26a3cd0f0074f123c789b3f147b97b86ece46a4
Uko Uwatt <uwattuko@gmail.com>
2023-03-31 02:54:20 -0700

    correction has been made

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,7 +16,7 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil\n)");
+			printf("[0] (ni)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
